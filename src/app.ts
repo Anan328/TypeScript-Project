cd4132437f4952bafbb80436f8887fd422ed30b7
Anan328 <ananshah328@gmail.com>
2024-12-27 23:30:42 +0530

    The DOM & Type Casting

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,19 @@
-let x : string = 'hello';
+const anchor = document.querySelector('a')!; // handles null errors
+const ul = document.querySelector('.item-list')!;
+const form = document.querySelector('.new-item-form') as HTMLFormElement; // The typecasting tells TypeScript that these element is specifically a form
+const type = document.querySelector('#type')as HTMLSelectElement; // The typecasting tells TypeScript that these elements are specifically select elements
+const ToFrom = document.querySelector('#tofrom')as HTMLInputElement;
+const details = document.getElementById('details')as HTMLInputElement;
+const amount = document.getElementById('amount')!as HTMLInputElement;
+const button = document.querySelector('button')!; 
 
-console.log(x);
+form.addEventListener('submit',(e:Event)=>{
+    e.preventDefault();
+    const li = document.createElement('li');
+    li.innerHTML = `<b>Type:</b> ${type.value} <b>Name:</b> ${ToFrom.value} <b>Details:</b> ${details.value} <b>Amount:</b> ${amount.valueAsNumber} `; // here valueAsNumber turns it into a number because js by default converts it to the string
+    ul.appendChild(li);
+    console.log('test');
+    
+    
+})
+
