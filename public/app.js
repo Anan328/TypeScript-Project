099da20f2ab9952e946701df62fa9effb176b3a3
Anan328 <ananshah328@gmail.com>
2024-12-27 23:58:29 +0530

    Classes

--- public/app.js
+++ public/app.js
@@ -1,4 +1,23 @@
 "use strict";
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes ${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+console.log(invOne, invTwo);
+let invoices = []; // only objects created using Invoice class can be added to this array
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invoices);
+// invoices.push("hello"); // not valid
+// invoices.push({name:'test'}) // not valid
 const anchor = document.querySelector('a'); // handles null errors
 const ul = document.querySelector('.item-list');
 const form = document.querySelector('.new-item-form'); // The typecasting tells TypeScript that these element is specifically a form
@@ -10,7 +29,7 @@ const button = document.querySelector('button');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const li = document.createElement('li');
-    li.innerHTML = `<b>Type:</b> ${type.value} <b>Name:</b> ${ToFrom.value} <b>Details:</b> ${details.value} <b>Amount:</b> ${amount.valueAsNumber} `; // here valueAsNumber turns it into a number because js by default converts it to the string
+    li.innerHTML = `<b>Type:</b> ${type.value} <b>Name:</b> ${ToFrom.value} <b>Details:</b> ${details.value} <b>Amount (£):</b> ${amount.valueAsNumber} `; // here valueAsNumber turns it into a number because js by default converts it to the string
     ul.appendChild(li);
     console.log('test');
 });
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,38 @@
+class Invoice{
+    private client: string;
+    private details: string;
+    readonly amount: number;
+
+    constructor(c : string, d : string, a : number){
+        this.client = c;
+        this.details = d;
+        this.amount = a;    
+    }
+
+    format(){
+        return `${this.client} owes ${this.amount} for ${this.details}`
+    }
+}
+
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+
+console.log(invOne, invTwo);
+
+let invoices : Invoice[] = []; // only objects created using Invoice class can be added to this array
+
+invoices.push(invOne);
+invoices.push(invTwo);
+
+console.log(invoices);
+
+
+// invoices.push("hello"); // not valid
+// invoices.push({name:'test'}) // not valid
+
+
+
+
 const anchor = document.querySelector('a')!; // handles null errors
 const ul = document.querySelector('.item-list')!;
 const form = document.querySelector('.new-item-form') as HTMLFormElement; // The typecasting tells TypeScript that these element is specifically a form
@@ -10,7 +45,7 @@ const button = document.querySelector('button')!;
 form.addEventListener('submit',(e:Event)=>{
     e.preventDefault();
     const li = document.createElement('li');
-    li.innerHTML = `<b>Type:</b> ${type.value} <b>Name:</b> ${ToFrom.value} <b>Details:</b> ${details.value} <b>Amount:</b> ${amount.valueAsNumber} `; // here valueAsNumber turns it into a number because js by default converts it to the string
+    li.innerHTML = `<b>Type:</b> ${type.value} <b>Name:</b> ${ToFrom.value} <b>Details:</b> ${details.value} <b>Amount (£):</b> ${amount.valueAsNumber} `; // here valueAsNumber turns it into a number because js by default converts it to the string
     ul.appendChild(li);
     console.log('test');
     
